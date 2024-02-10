b109915ba217b397cdb6a12b098b16ee3eb218ae
Jarek Krochmalski <jotka@users.noreply.github.com>
2024-02-10 06:28:48 +0100

    Update update_docker_images.zsh

--- update_docker_images.zsh
+++ update_docker_images.zsh
@@ -1,22 +1,36 @@
 #!/bin/zsh
+#
+# Docker Update Check and Notification Script
+#
+# This script checks for updates in Docker containers using watchtower,
+# and sends a notification via Apprise if updates are found. The notification
+# includes the number of updated containers and the detailed output from watchtower.
+#
+# Make sure to set your Apprise notification service URL (APPRISE_URL) before running this script.
+#
 
 # Set your Apprise notification service URL
 APPRISE_URL="https://apprise.url.here"
 
+# Specify the watchtower image
+WATCHTOWER_IMAGE="docker.io/containrrr/watchtower:latest"
+
 # Function to check for updates and send notification
-update_images() {
+check_and_notify_updates() {
     local updated_containers
 
     # Function to check for updates and update containers
     check_and_update_containers() {
         echo "Checking for updates..."
-        updated_containers=$(docker run --rm -v /var/run/docker.sock:/var/run/docker.sock docker.io/containrrr/watchtower:latest --run-once)
+        updated_containers=$(docker run --rm -v /var/run/docker.sock:/var/run/docker.sock $WATCHTOWER_IMAGE --run-once 2>&1)
+        echo "watchtower output: $updated_containers"
     }
 
     # Function to send notification using curl
     send_notification() {
         local title="$1"
         local body="$2"
+        
         curl -k -X POST -F "title=$title" -F "body=$body" "$APPRISE_URL"
     }
 
@@ -24,7 +38,7 @@ update_images() {
     check_and_update_containers
 
     # Extract the number of updated containers from the output
-    num_updated_containers=$(echo "$updated_containers" | grep -o "Updated=[0-9]*" | awk -F= '{print $2}')
+    num_updated_containers=$(echo "$updated_containers" | grep -oP 'Updated=\K\d+')
 
     # Check if there were updates
     if [ "$num_updated_containers" -gt 0 ]; then
@@ -36,4 +50,4 @@ update_images() {
 }
 
 # Call the function
-update_images
+check_and_notify_updates
